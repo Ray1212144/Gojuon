21514a48e43836f3c6f1e9b7eb25c25f971b24b1
Ray1212144 <147676185+Ray1212144@users.noreply.github.com>
2025-11-17 12:54:39 -0500

    timeline

---
+++ scene/main.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name Main
+
+@export var canvas_animation_player : AnimationPlayer
+
+
+func _ready() -> void:
+	canvas_animation_player.play("blackout")
+	Dialogic.signal_event.connect(_on_dialogic_text_signal)
+	#Dialogic.start("intro_1")
+	
+
+func _on_dialogic_text_signal(argument:String):
+	if argument == "blackout":
+		canvas_animation_player.play("blackout")
+		
+		
+#func start_dialog():
+	#Dialogic.timeline_ended.connect(_on_timeline_ended)
+	#Dialogic.start("my_timeline")
+#
+#func _on_timeline_ended():
+	#Dialogic.timeline_ended.disconnect(_on_timeline_ended)
