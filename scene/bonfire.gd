d407425e897171fae7ac7f1d0833d94681a8b195
Ray1212144 <147676185+Ray1212144@users.noreply.github.com>
2025-11-18 15:35:52 -0500

    fire area

--- scene/bonfire.gd
+++ scene/bonfire.gd
@@ -6,6 +6,7 @@ class_name Bonfire
 var main : Main
 @export var place : Node2D
 @export var file_path:String
+var player_entered : bool
 
 # 标识此篝火是否为当前激活的复活点
 var is_active: bool = false
@@ -18,17 +19,29 @@ func call_reset():
 	main.reset(main.get_path_to(place.get_parent()),file_path)
 
 
+
 func _process(delta: float) -> void:
 	if is_active:
 		self_modulate = Color.ORANGE
 	else:
 		self_modulate = Color.WHITE
 
-
 func _on_button_pressed() -> void:
+	if not player_entered:
+		return
 	var player :Player = get_tree().get_first_node_in_group("player")
 	player.bonfire = self
 	var fire = get_tree().get_nodes_in_group("bonfire")
 	for i:Bonfire in fire:
 		i.is_active = false 
 	is_active = true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_entered = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_entered = false
